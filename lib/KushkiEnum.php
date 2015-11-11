e5c33b6cd3b6685940a3954a51ae9c6451df6a3e
pc84 <pedroecastineiras@gmail.com>
2015-11-11 16:40:36 -0500

    creating class KushkiCurrencys and KushkiLanguages for handle all enumerator in library

---
+++ lib/KushkiEnum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace kushki\lib;
+
+//use SplEnum;
+
+class KushkiLanguages //extends \SplEnum
+{
+    const
+        __default = 'es',
+        ES = 'es',
+        EN = 'en';
+}
+
+class KushkiCurrencys //extends SplEnum
+{
+    const
+        __default = 'USD',
+        USD = 'USD';
+}
+
+?>
