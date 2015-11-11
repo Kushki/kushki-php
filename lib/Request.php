07455bd88ef7e62d03d14c400f1b6bc8d52f14fd
pc84 <pedroecastineiras@gmail.com>
2015-11-11 16:18:01 -0500

    creating Request class for encapsulate requests

---
+++ lib/Request.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace kushki\lib;
+
+use kushki\lib\KushkiConstant;
+
+class Request
+{
+
+    protected $url = "";
+    protected $params = array();
+    protected $contentType = "";
+
+    public function __construct($url, $params = array(),
+                                $contentType = KushkiConstant::CONTENT_TYPE)
+    {
+        $this->url = $url;
+        $this->params = $params;
+        $this->contentType = $contentType;
+    }
+
+    public function getParameter($parameterName)
+    {
+        if (isset($this->params[$parameterName])) {
+            return $this->params[$parameterName];
+        }
+        throw new KushkiException(KushkiConstant::PARAMETER_DO_NOT_EXIST);
+    }
+
+    public function getContentType()
+    {
+        return $this->contentType;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+}
+
+?>
