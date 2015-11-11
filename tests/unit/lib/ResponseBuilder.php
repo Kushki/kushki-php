8d055c59d168c8d8fb97a10926160fd3cab00784
pc84 <pedroecastineiras@gmail.com>
2015-11-11 16:21:13 -0500

    creating class ResponseBuilder for generate Response for mock test

---
+++ tests/unit/lib/ResponseBuilder.php
@@ -0,0 +1,32 @@
+<?php
+namespace kushki\tests\unit\lib;
+
+use kushki\lib\Response;
+use kushki\lib\KushkiConstant;
+
+class ResponseBuilder
+{
+
+    static public function createChargeOKResponse()
+    {
+        $randomTransactionId = Utils::randomAlphaNumberString(15, 40);
+        $body = array(KushkiConstant::PARAMETER_TRANSACTION_ID => $randomTransactionId);
+        $jsonBody = json_encode($body);
+        $response = new Response(KushkiConstant::CONTENT_TYPE, $jsonBody, 200);
+        return $response;
+    }
+
+    static public function createChargeFailedResponse()
+    {
+        $randomErrorMessage = Utils::randomAlphaNumberString(15, 40);
+
+        $body = array(KushkiConstant::PARAMETER_ERRORS =>
+            array(KushkiConstant::PARAMETER_ERRORS_MESSAGE =>
+                $randomErrorMessage),
+            KushkiConstant::PARAMETER_ERRORS_CODE => rand(1, 10));
+        $jsonBody = json_encode($body);
+        $response = new Response(KushkiConstant::CONTENT_TYPE, $jsonBody, 402);
+        return $response;
+    }
+
+}
