d332a65afa2a01cedb289f2003190e27c3bd9bd8
pc84 <pedroecastineiras@gmail.com>
2015-11-11 16:18:18 -0500

    creating Response class for encapsulate responses

---
+++ lib/Response.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace kushki\lib;
+
+class Response
+{
+
+    protected $contentType = "";
+    protected $body;
+    protected $responseCode;
+
+    public function __construct($contentType, $body, $response_code)
+    {
+        $this->contentType = $contentType;
+        $this->body = $body;
+        $this->responseCode = $response_code;
+    }
+
+    public function getContentType()
+    {
+        return $this->contentType;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function getResponseCode()
+    {
+        return $this->responseCode;
+    }
+
+}
+
+?>
