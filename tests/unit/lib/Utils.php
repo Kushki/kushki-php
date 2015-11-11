b242b698035aa6b8cc283b7ae88fa50a5f25b7d7
pc84 <pedroecastineiras@gmail.com>
2015-11-11 16:20:25 -0500

    adding Utils class for generate random values needed in test

---
+++ tests/unit/lib/Utils.php
@@ -0,0 +1,29 @@
+<?php
+namespace kushki\tests\unit\lib;
+
+class Utils extends \PHPUnit_Framework_TestCase
+{
+
+    const ALPHA_NUMBER_CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const ALPHA_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    static function randomAlphaNumberString($minSize = 1, $maxSize = 10)
+    {
+        return self::randomString(self::ALPHA_NUMBER_CHARACTERS, $minSize, $maxSize);
+    }
+
+    static function randomAlphaString($minSize = 1, $maxSize = 10)
+    {
+        return self::randomString(self::ALPHA_CHARACTERS, $minSize, $maxSize);
+    }
+
+    static function randomString($alphabet, $minSize = 1, $maxSize = 10)
+    {
+        $randstring = '';
+        for ($i = $minSize; $i <= $maxSize; $i++) {
+            $randstring = $alphabet[rand(0, strlen($alphabet) - 1)];
+        }
+        return $randstring;
+    }
+
+}
