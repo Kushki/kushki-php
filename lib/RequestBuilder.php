660160769700f58d44c6b883286a8b55d31443c2
pc84 <pedroecastineiras@gmail.com>
2015-11-18 11:23:45 -0500

    Remove createTokenRequest function in builder

--- lib/RequestBuilder.php
+++ lib/RequestBuilder.php
@@ -22,26 +22,4 @@ class RequestBuilder
         return $request;
     }
 
-    static public function createTokenRequest($url, $merchantId, $language, $cardName,
-                                              $cardNumber, $expiredMonth,
-                                              $expiredYear, $cvc,
-                                              $deferred = false, $months = 0)
-    {
-        $cardInformation = array(KushkiConstant::PARAMETER_CARD_NAME => $cardName,
-            KushkiConstant::PARAMETER_CARD_NUMBER => $cardNumber,
-            KushkiConstant::PARAMETER_CARD_EXP_MONTH => $expiredMonth,
-            KushkiConstant::PARAMETER_CARD_EXP_YEAR => $expiredYear,
-            KushkiConstant::PARAMETER_CARD_CVC => $cvc);
-        $params = array(KushkiConstant::PARAMETER_CARD => $cardInformation,
-            KushkiConstant::PARAMETER_MERCHANT_ID => $merchantId,
-            KushkiConstant::PARAMETER_LANGUAGE => $language);
-        if ($deferred) {
-            $params[KushkiConstant::PARAMETER_DEFERRED] = true;
-            $params[KushkiConstant::PARAMETER_MONTHS] = $months;
-        }
-
-        $request = new Request($url, $params, KushkiConstant::CONTENT_TYPE);
-        return $request;
-    }
-
 }
