972e789309b657e69bddc44c45b516157c84f809
pc84 <pedroecastineiras@gmail.com>
2015-11-18 11:25:51 -0500

    Fixing mock test

--- tests/unit/lib/ChargeRequestHandlerTest.php
+++ tests/unit/lib/ChargeRequestHandlerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace kushki\tests\unit\lib;
 
+use kushki\lib\ChargeRequestHandler;
 use kushki\lib\kushkiEnums;
 use kushki\lib\Request;
 
@@ -10,28 +11,30 @@ class ChargeRequestHandlerTest extends \PHPUnit_Framework_TestCase
     public function testMustGet200ResponseCode()
     {
         $responseExpected = ResponseBuilder::createChargeOKResponse();
-        $mock = $this->prepareMock($responseExpected);
+        $curlHandlerMock = $this->prepareMock($responseExpected);
         $dummyRequest = $this->getDummyRequest();
+        $chargeRequestHandler = new ChargeRequestHandler($curlHandlerMock);
 
-        $response = $mock->charge($dummyRequest);
+        $response = $chargeRequestHandler->charge($dummyRequest);
         $this->assertEquals(200, $response->getResponseCode());
     }
 
     public function testMustGet402ResponseCode()
     {
         $responseExpected = ResponseBuilder::createChargeFailedResponse();
-        $mock = $this->prepareMock($responseExpected);
+        $curlHandlerMock = $this->prepareMock($responseExpected);
         $dummyRequest = $this->getDummyRequest();
+        $chargeRequestHandler = new ChargeRequestHandler($curlHandlerMock);
 
-        $response = $mock->charge($dummyRequest);
+        $response = $chargeRequestHandler->charge($dummyRequest);
         $this->assertEquals(402, $response->getResponseCode());
     }
 
     private function prepareMock($responseExpected)
     {
-        $mock = $this->getMockBuilder('ChargeRequestHandler')->setMethods(array('charge'))->getMock();
+        $mock = $this->getMockBuilder('CurlHandler')->setMethods(array('call'))->getMock();
 
-        $mock->method('charge')->willReturn($responseExpected);
+        $mock->method('call')->willReturn($responseExpected);
 
         return $mock;
     }
