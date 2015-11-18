e7770c0bda7d73e837af416168203f43036ea491
pc84 <pedroecastineiras@gmail.com>
2015-11-18 11:00:08 -0500

    Adding set time zone to autoload for fix phpunit code coverage bug

--- autoload.php
+++ autoload.php
@@ -9,11 +9,11 @@ require_once(dirname(__FILE__) . '/lib/Response.php');
 require_once(dirname(__FILE__) . '/lib/CurlHandler.php');
 require_once(dirname(__FILE__) . '/lib/RequestHandler.php');
 require_once(dirname(__FILE__) . '/lib/RequestBuilder.php');
-require_once(dirname(__FILE__) . '/lib/TokenRequestHandler.php');
 require_once(dirname(__FILE__) . '/lib/ChargeRequestHandler.php');
 
 #only for testing
 require_once(dirname(__FILE__) . '/tests/unit/lib/Utils.php');
 require_once(dirname(__FILE__) . '/tests/unit/lib/ResponseBuilder.php');
 
+date_default_timezone_set('America/Los_Angeles');
 ?>
