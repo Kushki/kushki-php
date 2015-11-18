3485366cc346278c2d5381c44e87d97d00bbd206
pc84 <pedroecastineiras@gmail.com>
2015-11-18 11:25:16 -0500

    Adding integration test in kushki class

--- tests/inttests/lib/KushkiIntTest.php
+++ tests/inttests/lib/KushkiIntTest.php
@@ -1,10 +1,45 @@
 <?php
 namespace kushki\tests\inttests\lib;
 
+use kushki\lib\Kushki;
+use kushki\lib\KushkiConstant;
+use kushki\lib\KushkiCurrencies;
+use kushki\lib\KushkiLanguages;
+use kushki\lib\RequestBuilder;
+
 class KushkiIntTest extends \PHPUnit_Framework_TestCase
 {
-    public function testHasMerchantId()
+
+    public function testMustGet200ResponseCodeWhenChargeBeOk()
     {
-        $this->assertEquals(0, 0, "Cero debe ser Cero");
+        $successToken = "d0099d11-f443-4e54-b7aa-6ede163c94c9";
+        $request = $this->createRequest($successToken);
+        $kushki = new Kushki($request->getParameter(KushkiConstant::PARAMETER_MERCHANT_ID),
+                                $request->getParameter(KushkiConstant::PARAMETER_LANGUAGE),
+                                $request->getParameter(KushkiConstant::PARAMETER_CURRENCY_CODE));
+
+        $response = $kushki->charge($successToken, $request->getParameter(KushkiConstant::PARAMETER_TRANSACTION_AMOUNT));
+        $this->assertEquals(200, $response->getResponseCode());
+    }
+
+    public function testMustGet402ResponseCodeWhenChargeBeDeclined()
+    {
+        $declinedToken = "123456789-declined";
+        $request = $this->createRequest($declinedToken);
+        $kushki = new Kushki($request->getParameter(KushkiConstant::PARAMETER_MERCHANT_ID),
+            $request->getParameter(KushkiConstant::PARAMETER_LANGUAGE),
+            $request->getParameter(KushkiConstant::PARAMETER_CURRENCY_CODE));
+
+        $response = $kushki->charge($declinedToken, $request->getParameter(KushkiConstant::PARAMETER_TRANSACTION_AMOUNT));
+        $this->assertEquals(402, $response->getResponseCode());
+    }
+
+    private function createRequest($token)
+    {
+        $amount = rand(1, 1000);
+        $currency = KushkiCurrencies::USD;
+        $merchantId = "MERCHANT-PRIVATE-ID";
+        $request = RequestBuilder::createChargeRequest(KushkiConstant::CHARGE_URL, $token, $amount, $currency, $merchantId, KushkiLanguages::ES);
+        return $request;
     }
 }
