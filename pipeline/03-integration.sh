cd5377aba4d5aadcb31634c8b73d18d60870cd9f
Luz <luzma_87@yahoo.com>
2016-02-17 17:09:13 -0500

    +@vrokida changing unzip folder

--- pipeline/03-integration.sh
+++ pipeline/03-integration.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 composer install --no-interaction
-echo "ls ../"
-ls ../
-mkdir ../kushkiTest
-cp -f out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip ../kushkiTest
-echo "ls ../kushkiTest antes de unzip"
-ls ../kushkiTest
-unzip -o ../kushkiTest/kushki-$SNAP_PIPELINE_COUNTER.zip
-echo "ls ../kushkiTest despues de unzip"
-ls ../kushkiTest
-cp -rf tests ../kushkiTest
-cp -f phpunit.xml ../kushkiTest
-cd ../kushkiTest
+echo "ls "
+ls
+mkdir kushkiTest
+cp -f out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip kushkiTest
+echo "ls kushkiTest antes de unzip"
+ls kushkiTest
+unzip -o kushkiTest/kushki-$SNAP_PIPELINE_COUNTER.zip
+echo "ls kushkiTest despues de unzip"
+ls kushkiTest
+cp -rf tests kushkiTest
+cp -f phpunit.xml kushkiTest
+cd kushkiTest
 echo "ls antes de tests"
 ls
 phpunit --configuration phpunit.xml --testsuite integration
