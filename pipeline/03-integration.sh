b4e6027fe0f723178501d0f440eca7d60ab6119b
Luz <luzma_87@yahoo.com>
2016-02-17 15:36:01 -0500

    +@vrokida adding logs to debug integration step

--- pipeline/03-integration.sh
+++ pipeline/03-integration.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 composer install --no-interaction
+echo "ls ../"
 ls ../
 mkdir ../kushkiTest
 cp out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip ../kushkiTest
+echo "ls ../kushkiTest antes de unzip"
 ls ../kushkiTest
 unzip ../kushkiTest/kushki-$SNAP_PIPELINE_COUNTER.zip
+echo "ls ../kushkiTest despues de unzip"
 ls ../kushkiTest
 cp -r tests ../kushkiTest
 cp phpunit.xml ../kushkiTest
 cd ../kushkiTest
+echo "ls antes de tests"
 ls
 phpunit --configuration phpunit.xml --testsuite integration
