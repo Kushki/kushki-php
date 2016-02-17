c5dd79b449009fb2281f0b2fa508b2cedf92ef50
Luz <luzma_87@yahoo.com>
2016-02-17 17:03:59 -0500

    +@vrokida adding force override to integration step

--- pipeline/03-integration.sh
+++ pipeline/03-integration.sh
@@ -3,14 +3,14 @@ composer install --no-interaction
 echo "ls ../"
 ls ../
 mkdir ../kushkiTest
-cp out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip ../kushkiTest
+cp -f out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip ../kushkiTest
 echo "ls ../kushkiTest antes de unzip"
 ls ../kushkiTest
-unzip ../kushkiTest/kushki-$SNAP_PIPELINE_COUNTER.zip
+unzip -o ../kushkiTest/kushki-$SNAP_PIPELINE_COUNTER.zip
 echo "ls ../kushkiTest despues de unzip"
 ls ../kushkiTest
-cp -r tests ../kushkiTest
-cp phpunit.xml ../kushkiTest
+cp -rf tests ../kushkiTest
+cp -f phpunit.xml ../kushkiTest
 cd ../kushkiTest
 echo "ls antes de tests"
 ls
