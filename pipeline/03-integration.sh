b3416a0401ae451a4a24a27dfa599981989acd24
Luz <luzma_87@yahoo.com>
2016-02-17 17:23:39 -0500

    +@vrokida removing logs from integration step

--- pipeline/03-integration.sh
+++ pipeline/03-integration.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 composer install --no-interaction
-echo "ls "
-ls
 mkdir kushkiTest
 cp -f out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip kushkiTest
-echo "ls kushkiTest antes de unzip"
 cd kushkiTest
-ls
 unzip -o kushki-$SNAP_PIPELINE_COUNTER.zip
-echo "ls kushkiTest despues de unzip"
-ls
 cp -rf ../tests .
 cp -f ../phpunit.xml .
-echo "ls antes de tests"
-ls
 phpunit --configuration phpunit.xml --testsuite integration
