9bdb9d14872bb56036adef0813ac7973165ba339
Luz <luzma_87@yahoo.com>
2016-03-23 10:37:37 -0500

    +@crisguitar Adding kushki zip file to distribution folder

--- pipeline/02-artifact.sh
+++ pipeline/02-artifact.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 zip -r out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip * --exclude='*.git*' --exclude='*test*' --exclude='README.md' --exclude='runTest.sh' --exclude='*.iml*' --exclude='phpunit.xml' --exclude='*reports*' --exclude='*pipeline*'
+
+# ARTIFACTS:
+# ARTIFACT out/artifacts/Kushki/
--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cp out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip out/artifacts/Kushki/kushki.zip
 git add out/artifacts/Kushki/
 git commit -m '[snap-ci] adding latest version of zip, pipeline '$SNAP_PIPELINE_COUNTER
 git config --global push.default simple
