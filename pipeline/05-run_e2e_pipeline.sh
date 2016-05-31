7d17839cb3cee78ccd125d689205d164aabd0c22
vrokida <vrokida@gmail.com>
2016-05-31 09:54:09 -0500

    +@luzma87 [KFE-116] Adding to trigger e2e pipeline

---
+++ pipeline/05-run_e2e_pipeline.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -u vrokida:muplE9Ctid7PCWnxVzZjR4zUsCnXiNKHIGSCvWwlm-8 -X POST -H 'Accept: application/vnd.snap-ci.com.v1+json' -H 'Content-type: application/json' https://api.snap-ci.com/project/Kushki/e2e-tests/branch/master/trigger
