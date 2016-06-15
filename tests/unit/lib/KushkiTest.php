ff9318b9354e0f6a1eba8dd82da441dded312b08
Luz <luzma_87@yahoo.com>
2016-06-15 14:52:28 -0500

    +@jreyes33 [KFE-111] Add unit test cases for Kushki class

--- tests/unit/lib/KushkiTest.php
+++ tests/unit/lib/KushkiTest.php
@@ -1,17 +1,65 @@
 <?php
 namespace kushki\tests\unit\lib;
 
+use kushki\lib\Amount;
 use kushki\lib\KushkiCurrencies;
 use kushki\lib\KushkiLanguages;
 use kushki\lib\Kushki;
+use kushki\lib\RequestHandler;
+use kushki\tests\lib\CommonUtils;
 use PHPUnit_Framework_TestCase;
+use ReflectionProperty;
 
 class KushkiTest extends PHPUnit_Framework_TestCase {
+    private $environment;
+    private $kushki;
+    private $merchantId;
+    private $requestHandler;
+    private $actionUrl;
+
+    protected function setUp() {
+        $this->environment = CommonUtils::randomAlphaString(5, 10);
+        $this->merchantId = rand(1000, 10000);
+        $this->kushki = new Kushki($this->merchantId, KushkiLanguages::ES, KushkiCurrencies::USD, $this->environment);
+        $this->requestHandler = $this->getMockBuilder(RequestHandler::class)
+                                     ->setMethods(array('call'))
+                                     ->getMock();
+        $this->overrideRequestHandler($this->kushki, $this->requestHandler);
+    }
 
     public function testHasMerchantId() {
-        $merchantId = rand(1000, 10000);
-        $api = new Kushki($merchantId, KushkiLanguages::ES, KushkiCurrencies::USD);
+        $this->assertEquals($this->merchantId, $this->kushki->getMerchantId(), "Does not have merchant id");
+    }
 
-        $this->assertEquals($merchantId, $api->getMerchantId(), "Does not have merchant id");
+    public function testChargeHasCorrectUrl() {
+        $this->actionUrl = '/charge';
+        $this->assertUrl();
+        $this->kushki->charge("a", new Amount(1, 1, 1, 1));
+    }
+
+    public function testDeferredChargeHasCorrectUrl() {
+        $this->actionUrl = '/deferred';
+        $this->assertUrl();
+        $this->kushki->deferredCharge("a", new Amount(1, 1, 1, 1), 3);
+    }
+
+    public function testVoidHasCorrectUrl() {
+        $this->actionUrl = '/void';
+        $this->assertUrl();
+        $this->kushki->voidCharge("a", new Amount(1, 1, 1, 1));
+    }
+
+    private function overrideRequestHandler($kushki, $requestHandler) {
+        $requestHandlerProperty = new ReflectionProperty(Kushki::class, 'requestHandler');
+        $requestHandlerProperty->setAccessible(true);
+        $requestHandlerProperty->setValue($kushki, $requestHandler);
+    }
+
+    private function assertUrl() {
+        $this->requestHandler->expects($this->once())
+                             ->method('call')
+                             ->will($this->returnCallback(function ($request) {
+                                 $this->assertEquals($this->environment . $this->actionUrl, $request->getUrl());
+                             }));
     }
 }
