f3b750ca5e6a4ac26ef329e61187de24e99a078e
Luz <luzma_87@yahoo.com>
2016-07-11 16:39:15 -0500

    +@jreyes33 [KFE-106] Fix repository url in distribute step

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -11,10 +11,11 @@ git commit -m "[snap-ci] adding latest version of zip, version: $artifact_versio
 git config --global push.default simple
 git pull --rebase
 git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
+git push
 git push --tags
 curl -X POST -H 'content-type:application/json' \
     "https://packagist.org/api/update-package?username=kushki&apiToken=$PACKAGIST_API_TOKEN" \
-    -d '{"repository":{"url":"https://packagist.org/packages/kushki/kushki-php"}}'
+    -d '{"repository":{"url":"https://github.com/Kushki/kushki-php"}}'
 
 
 # ENVS:
