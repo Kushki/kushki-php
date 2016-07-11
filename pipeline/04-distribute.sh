6eec7a3449088db306e83f15881de7d9cc848c50
Luz <luzma_87@yahoo.com>
2016-07-11 15:48:44 -0500

    +@jreyes33 [KFE-106] Configure pipeline to release to Packagist

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
-cp out/artifacts/Kushki/kushki-$SNAP_PIPELINE_COUNTER.zip out/artifacts/Kushki/kushki.zip
+
+artifact_version=$(cat composer.json | grep \"version\": | cut -d'"' -f 4)
+
+mv out/artifacts/Kushki/kushki-dev.zip out/artifacts/Kushki/kushki-$artifact_version.zip
 git add out/artifacts/Kushki/
-git commit -m '[snap-ci] adding latest version of zip, pipeline '$SNAP_PIPELINE_COUNTER
+git commit -m "[snap-ci] adding latest version of zip, version: $artifact_version"
 git config --global push.default simple
 git pull --rebase
-git push
+git tag --annotate "v$artifact_version"
+git push --tags
+curl -X POST -H 'content-type:application/json' \
+    "https://packagist.org/api/update-package?username=kushki&apiToken=$PACKAGIST_API_TOKEN" \
+    -d '{"repository":{"url":"https://packagist.org/packages/kushki/kushki-php"}}'
+
+
+# ENVS:
+# PACKAGIST_API_TOKEN
