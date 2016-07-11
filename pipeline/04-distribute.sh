d92f300c0ec73ff8658abe34dce91b417e7eef8d
Luz <luzma_87@yahoo.com>
2016-07-11 16:16:58 -0500

    +@jreyes33 [KFE-106] Provide a message for git tag

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 artifact_version=$(cat composer.json | grep \"version\": | cut -d'"' -f 4)
 
 cp out/artifacts/Kushki/kushki-dev.zip out/artifacts/Kushki/kushki.zip
@@ -8,7 +10,7 @@ git add out/artifacts/Kushki/
 git commit -m "[snap-ci] adding latest version of zip, version: $artifact_version"
 git config --global push.default simple
 git pull --rebase
-git tag --annotate "v$artifact_version"
+git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
 git push --tags
 curl -X POST -H 'content-type:application/json' \
     "https://packagist.org/api/update-package?username=kushki&apiToken=$PACKAGIST_API_TOKEN" \
