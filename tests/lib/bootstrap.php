67b9756b62926d5cf8b8ce9c5f171fd1997b5ba9
Luz <luzma_87@yahoo.com>
2016-07-12 10:38:31 -0500

    +@jreyes33 [KFE-106] adding autoload for users w/o composer

--- tests/lib/bootstrap.php
+++ tests/lib/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+// Also test with:
+// require_once __DIR__ . '/../../autoload.php';
+
 date_default_timezone_set('America/Guayaquil');
